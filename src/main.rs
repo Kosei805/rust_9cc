f5648f988d9bfa30b17d236115960cd642f4d600
Kosei Nakazono <na913420ka85@gmail.com>
2023-09-11 13:33:47 +0900

    compiler 与えた値を戻り値として返す

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        panic!("引数の個数が正しくありません:{}",args.len());
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("\tmov rax, {}",&args[1]);
+    println!("\tret");
 }
